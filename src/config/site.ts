dee6948d87260cf8c9f5591b232efea4ff95a2e1
Cristiana Chavez <cristianachavez214@gmail.com>
2024-09-05 13:53:15 +0800

    update description details

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,27 +3,29 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "Marketplace",
   description:
-    " Welcome to NanayNitz Fresh Veggies Farm your one-stop shop for the finest locally grown produce! We offer a wide variety of farm-fresh products straight from our farm.All our products are sustainably grown, ensuring both quality and freshness in every bite. Experience the best nature has to offer, delivered straight to your doorstep.",
+    "Welcome to NanayNitz Fresh Veggies Farm your one-stop shop for the finest locally grown produce! We offer a wide variety of farm-fresh products straight from our farm. All our products are sustainably grown, ensuring both quality and freshness in every bite. Experience the best nature has to offer, delivered straight to your doorstep.",
   mainNav: [
     {
       title: "Categories",
       href: "/genre",
     },
     {
-      title: "JavaScript",
-      href: "/js",
+      title: "Farm tools ",
+      href: "/tools",
     },
     {
-      title: "AI",
-      href: "/ai",
+      title: "Vegetables",
+      href: "/genre/vegetables/",
+    },
+    {
+      title: "Banana",
+      href: "/item/banana/",
     },
   ],
   links: {
     twitter: "https://twitter.com/devcodingmemes",
-    twitterDev: "https://twitter.com/cristiana214",
-    github: "https://github.com/cristiana214",
-    lens: "https://hey.xyz/u/codingmemes",
-    farcaster: "https://warpcast.com/codingmemes",
-    facebook: "https://facebook.com/devcodingmemes",
+
+    github: "https://github.com/cristiana214/marketplace",
+    facebook: "https://www.facebook.com/freshveggiesfarm",
   },
 };
