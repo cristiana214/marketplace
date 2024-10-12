2ca8d05cd265bdf3d0e67d1100d5dbd83f185cfb
Cristiana Chavez <cristianachavez214@gmail.com>
2024-10-13 02:19:29 +0800

    Add actual products api

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,17 +1,51 @@
 import { db } from "@/drizzle/db";
 import { eq } from "drizzle-orm";
-import { categoriesTb, categoryTypesTb, productsTb } from "@/drizzle/schema";
+import {
+  categoriesTb,
+  categoryTypesTb,
+  productsTb,
+  productUnitTb,
+} from "@/drizzle/schema";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const categoryUrl = req.nextUrl.searchParams.get("category_url");
-    const typeUrl = req.nextUrl.searchParams.get("type_url");
+    const categoryUrl = req.nextUrl.searchParams.get("categoryUrl");
+    const categoryTypeUrl = req.nextUrl.searchParams.get("categoryTypeUrl");
 
     let products;
     if (categoryUrl) {
       // get all products by category_url
+      products = await db
+        .select({
+          productId: productsTb.product_id,
+          name: productsTb.name,
+          // url: productsTb.url,
+          price: productsTb.price,
+          unitDisplayName: productUnitTb.display_name,
+          quantity: productsTb.quantity_available,
+          description: productsTb.description,
+          typeId: productsTb.type_id,
+          // imageUrl: productsTb.image;
+          // images: string[];
+        })
+        .from(productsTb)
+        .innerJoin(
+          categoryTypesTb,
+          eq(productsTb.type_id, categoryTypesTb.type_id),
+        )
+        .innerJoin(
+          categoriesTb,
+          eq(categoriesTb.category_id, categoryTypesTb.category_id),
+        )
+        .innerJoin(
+          productUnitTb,
+          eq(productUnitTb.unit_type_id, productsTb.unit_type_id),
+        )
+        .where(eq(categoriesTb.url, String(categoryUrl)));
+    } else if (categoryTypeUrl) {
+      // get all products by type_url
       products = await db
         .select()
         .from(productsTb)
@@ -23,9 +57,14 @@ export async function GET(req: NextRequest) {
           categoriesTb,
           eq(categoriesTb.category_id, categoryTypesTb.category_id),
         )
-        .where(eq(categoriesTb.url, String(categoryUrl)));
-    } else if (typeUrl) {
-      // get all products by type_url
+        .innerJoin(
+          productUnitTb,
+          eq(productUnitTb.unit_type_id, productsTb.unit_type_id),
+        )
+        .where(eq(categoryTypesTb.url, String(categoryTypeUrl)));
+    } else {
+      // select all products
+      // /api/products/
       products = await db
         .select()
         .from(productsTb)
@@ -33,17 +72,21 @@ export async function GET(req: NextRequest) {
           categoryTypesTb,
           eq(productsTb.type_id, categoryTypesTb.type_id),
         )
-        .where(eq(categoryTypesTb.url, String(typeUrl)));
-    } else {
-      // select all products
-      // /api/products/
-      products = await db.select().from(productsTb);
+        .innerJoin(
+          categoriesTb,
+          eq(categoriesTb.category_id, categoryTypesTb.category_id),
+        )
+        .innerJoin(
+          productUnitTb,
+          eq(productUnitTb.unit_type_id, productsTb.unit_type_id),
+        );
     }
 
-    return NextResponse.json(products);
+    return NextResponse.json({ products });
   } catch (error) {
+    console.log(error);
     return NextResponse.json({
-      message: "Error fetching category types",
+      message: "Error fetching products",
       error,
     });
   }
