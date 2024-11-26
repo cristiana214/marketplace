2a4f3016abccf3a3c35f7e0021860e9c1500ee70
cristiana214 <cristianachavez214@gmail.com>
2024-11-27 01:26:35 +0800

    Implement add to cart in product page

--- src/app/product/[productURL]/page.tsx
+++ src/app/product/[productURL]/page.tsx
@@ -16,19 +16,21 @@ import Cards from "@/components/reusable/cards";
 // import { productFarmer } from "@/lib/data/product";
 import { useProduct } from "@/hooks/query/useProduct";
 import { generateUrl } from "@/lib/helper/generate-url";
+import { useCartStore } from "@/lib/store/useCartStore";
 
 export default function ProductPage({
   params,
 }: {
   params: { productURL?: string };
 }) {
-  const [quantity, setQuantity] = useState(1);
+  const [quantity, setQuantity] = useState(0);
   // handling the current active images on slide
   const [activeImage, setActiveImage] = useState(0);
 
   // set images from array string separated by comma
   const [images, setImages] = useState<string[]>([]);
   const [newComment, setNewComment] = useState("");
+  const { addToCart, removeFromCart } = useCartStore();
 
   const { data, isLoading, error } = useProduct({
     productId: params?.productURL,
@@ -44,6 +46,7 @@ export default function ProductPage({
   if (error) return <div>Error loading product</div>;
 
   const handleAddToCart = () => {
+    if (product) addToCart(product?.productId, product);
     console.log(`Added ${quantity} ${product?.name} to cart`);
   };
 
@@ -145,11 +148,22 @@ export default function ProductPage({
           <Button
             variant="outline"
             size="icon"
-            onClick={() => setQuantity(quantity + 1)}
+            onClick={() => {
+              setQuantity(quantity + 1);
+              handleAddToCart();
+            }}
           >
             <Plus className="size-4" />
           </Button>
-          <Button onClick={handleAddToCart}>Add to Cart</Button>
+
+          <Button
+            onClick={() => {
+              setQuantity(quantity + 1);
+              handleAddToCart();
+            }}
+          >
+            Add to Cart
+          </Button>
         </div>
         <Tabs defaultValue="farmer">
           <TabsList>
