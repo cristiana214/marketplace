68dcac5fc7d2f1f064f50a4a32dff134436f4dba
cristiana214 <cristianachavez214@gmail.com>
2024-11-26 22:25:50 +0800

    Include signup authentication.

--- src/components/form-signin.tsx
+++ src/components/form-signin.tsx
@@ -8,12 +8,13 @@ import type { FormEvent } from "react";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 interface CredentialsFormProps {
   csrfToken?: string;
 }
 
-export function FormCredentials(props: CredentialsFormProps) {
+export function FormSignin(props: CredentialsFormProps) {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
 
@@ -28,6 +29,8 @@ export function FormCredentials(props: CredentialsFormProps) {
     });
 
     if (signInResponse && !signInResponse.error) {
+      console.log("signInResponse");
+      console.log(signInResponse);
       // redirect to homepage (/)
       router.push("/");
     } else {
@@ -43,7 +46,6 @@ export function FormCredentials(props: CredentialsFormProps) {
           {error}
         </span>
       )}
-
       <Input
         type="email"
         name="email"
@@ -58,13 +60,18 @@ export function FormCredentials(props: CredentialsFormProps) {
         required
         className="mb-4 w-full rounded-md border border-gray-300 p-4"
       />
-
       <Button
         type="submit"
         className="focus:shadow-outline mt-4 h-12 w-full rounded-lg px-6 text-lg text-white transition-colors duration-150 dark:text-gray-700"
       >
         Sign In
       </Button>
+      <span className="mt-2 text-xs">
+        Don&apos;t have an account?{" "}
+        <Link className=" text-cyan-600 hover:font-semibold" href="/signup/">
+          Signup{" "}
+        </Link>
+      </span>
     </form>
   );
 }
--- src/drizzle/query/authentication.ts
+++ src/drizzle/query/authentication.ts
@@ -1,7 +1,14 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { and, eq } from "drizzle-orm";
 import { db } from "@/drizzle/db";
-import { userAuthTb, userImageTb, userTb } from "@/drizzle/schema";
+import {
+  userAuthTb,
+  userEmailTb,
+  userImageTb,
+  userLoginTb,
+  userPasswordTb,
+  userTb,
+} from "@/drizzle/schema";
 
 type CheckUserExist = {
   authId: string;
@@ -30,6 +37,7 @@ export const checkUserExist = async ({
         displayName: userTb.display_name,
         about: userTb.about,
         authId: userAuthTb.auth_id,
+        authEmail: userAuthTb.auth_email,
       })
       .from(userTb)
       .innerJoin(userAuthTb, eq(userAuthTb.user_id, userTb.user_id))
@@ -90,6 +98,7 @@ type InsertNewAuthUser = {
   authToken: string;
   imageUrl?: string;
 };
+
 export const insertNewAuthUser = async (userDB: InsertNewAuthUser) => {
   const {
     username,
@@ -128,7 +137,6 @@ export const insertNewAuthUser = async (userDB: InsertNewAuthUser) => {
       auth_token: String(authToken),
       auth_username: String(username),
     });
-    console.log(userDB);
     await db.insert(userImageTb).values({
       user_id: insertId,
       image_url: imageUrl || "",
@@ -139,5 +147,124 @@ export const insertNewAuthUser = async (userDB: InsertNewAuthUser) => {
   } catch (err) {
     console.error("Error inserting new auth user:", err);
     return { isSuccess: false, error: err };
+  }
+};
+type InsertNewSignupUser = {
+  authId?: string;
+  username?: string;
+  displayName: string;
+  authEmail?: string;
+  authTypeId: number;
+  authToken: string;
+  imageUrl?: string;
+  hashPassword: string;
+};
+export const insertNewSignupUser = async (userDB: InsertNewSignupUser) => {
+  const {
+    username,
+    displayName,
+    authEmail,
+    authId = 0,
+    authTypeId,
+    authToken,
+    imageUrl,
+    hashPassword,
+  } = userDB;
+
+  const active = 1;
+  const blocked = 0;
+  const imageType = 1;
+  const hashUserName = `${username}${Math.floor(Math.random() * 99)}`;
+
+  try {
+    await db.insert(userEmailTb).values({
+      user_email: String(authEmail),
+    });
+    // Insert into `user` table
+    const userInsertResult = await db.insert(userTb).values({
+      username: String(hashUserName),
+      display_name: String(displayName),
+      email: String(authEmail),
+      name: String(displayName),
+      user_type: 1, // customer
+      active: Boolean(active),
+      blocked: Boolean(blocked),
+    });
+    const insertId = userInsertResult?.[0]?.insertId;
+
+    // Insert into `user_auth` table
+    await db.insert(userAuthTb).values({
+      type_id: Number(authTypeId), // auth type id
+      user_id: Number(insertId),
+      auth_id: String(authId),
+      auth_email: String(authEmail),
+      auth_token: String(authToken),
+      auth_username: String(username),
+    });
+    await db.insert(userPasswordTb).values({
+      user_id: insertId,
+      hash_password: hashPassword,
+    });
+
+    await db.insert(userImageTb).values({
+      user_id: insertId,
+      image_url: imageUrl || "",
+      image_type_id: imageType,
+    });
+
+    await db.insert(userLoginTb).values({
+      type_id: 1,
+      user_id: insertId,
+      is_signup: 2,
+    });
+
+    return { isSuccess: true, user: userInsertResult };
+  } catch (err) {
+    console.log("Error inserting new local user:", err);
+    return { isSuccess: false, error: err };
+  }
+};
+
+// Define the response type
+type CheckEmailExistResponse = {
+  isSuccess: boolean;
+  userExist?: User;
+};
+type CheckEmailExist = {
+  email?: string;
+};
+export const checkEmailExist = async ({
+  email,
+}: CheckEmailExist): Promise<any> => {
+  try {
+    if (!email) return { isSuccess: false };
+    const userExist = await db
+      .select({
+        userId: userTb.user_id,
+        displayName: userTb.display_name,
+        about: userTb.about,
+        authId: userAuthTb.auth_id,
+        authEmail: userAuthTb.auth_email,
+        userPassword: userPasswordTb.hash_password,
+      })
+      .from(userTb)
+      .innerJoin(userAuthTb, eq(userAuthTb.user_id, userTb.user_id))
+      .innerJoin(userPasswordTb, eq(userAuthTb.user_id, userPasswordTb.user_id))
+      .where(
+        and(
+          eq(userTb.active, Boolean(1)),
+          eq(userAuthTb.auth_email, email),
+          eq(userAuthTb.type_id, 2),
+        ),
+      )
+      .groupBy(userTb.user_id);
+
+    return {
+      isSuccess: !!userExist?.length,
+      userExist,
+    };
+  } catch (error) {
+    console.log(error);
+    return { isSuccess: false };
   }
 };
