cfb85cea8d7a4217e07eed3e380636fa6bb1b6e8
cristiana214 <cristianachavez214@gmail.com>
2024-12-15 21:46:47 +0800

    Removed build errors

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -7,7 +7,6 @@ const pool = mysql.createPool({
   user: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  port: process.env.DATABASE_PORT,
 });
 // { logger: true }
 export const db = drizzle(pool, { logger: true });
