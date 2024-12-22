2c72fe88b32731d375b1688c84e9ec1c26fc3868
IC Marie Chavez <icitsice@gmail.com>
2024-12-23 01:25:27 +0800

    Updated design for landing page and user profile

--- src/config/site.ts
+++ src/config/site.ts
@@ -6,14 +6,6 @@ export const siteConfig = {
     "Welcome to Agri CBR your one-stop markertplace for the finest locally grown produce! We offer a wide variety of farm-fresh products straight from the farms. All our products are sustainably grown, ensuring both quality and freshness in every bite. Experience the best nature has to offer, delivered straight to your doorstep.",
   mainNav: [
     {
-      title: "Farms",
-      href: "/farm/nanay-nitz-fresh-veggies-farm/",
-    },
-    {
-      title: "Vegetables",
-      href: "/category/fruit-vegetables/",
-    },
-    {
       title: "Product",
       href: "/product/10/",
     },
@@ -26,10 +18,4 @@ export const siteConfig = {
       href: "/about/",
     },
   ],
-  links: {
-    twitter: "https://twitter.com/icitsice",
-
-    github: "https://github.com/cristiana214/marketplace",
-    facebook: "https://www.facebook.com/freshveggiesfarm",
-  },
 };
