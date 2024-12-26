13a685c6de571cd060cb1cf8c93bba36a603f86f
cristiana214 <cristianachavez214@gmail.com>
2024-12-27 00:05:34 +0800

    Added reusable footer on the main layout

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Leaf, Users, Truck, Heart, Info } from "lucide-react";
-import Footer from "../footer/page";
-import WhyChooseUs from "../whychoose/page";
+// import Footer from "../footer/page";
+// import WhyChooseUs from "../whychoose/page";
 
 export default function About() {
   return (
@@ -45,8 +45,8 @@ export default function About() {
           </div>
         </div>
       </div>
-      <WhyChooseUs />
-      <Footer />
+      {/* <WhyChooseUs /> */}
+      {/* <Footer /> */}
     </section>
   );
 }
