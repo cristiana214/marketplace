3dbe09463fea23f5386f29d7a31660f9b47af579
cristiana214 <cristianachavez214@gmail.com>
2025-01-12 00:57:24 +0800

    Add AddProduct page and refactor product handling in admin section

---
+++ src/app/admin/products/add/page.tsx
@@ -0,0 +1,9 @@
+import AddProduct from "@/components/admin/add-product";
+
+export default function AddProductPage() {
+  return (
+    <div className="flex justify-center bg-zinc-100">
+      <AddProduct />
+    </div>
+  );
+}
