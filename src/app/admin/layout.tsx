946edc68be456afc2d72ed2f3f5d6ca62feb3985
cristiana214 <cristianachavez214@gmail.com>
2025-01-24 02:37:55 +0800

    Refactor layout for Admin and User pages to improve responsiveness of NavOrders component

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <h1 className="mb-6 text-3xl font-bold">Admin Profile</h1>
       <ProfileHeader />
 
-      <div className="mb-6 mt-8 flex space-x-4">
+      <div className="mb-6 mt-8 flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
         <NavOrders userType={session?.user?.userType} />
       </div>
       {children}
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -9,7 +9,7 @@ export default function UserPage() {
     <div className="container mx-auto w-10/12 px-4 ">
       <h1 className="mb-6 text-3xl font-bold">My Profile</h1>
       <ProfileHeader />
-      <div className="mb-6 mt-8 flex space-x-4">
+      <div className="mb-6 mt-8 flex flex-col space-y-4 md:flex-row md:space-x-4 md:space-y-0">
         <NavOrders />
       </div>
       <ProductsList />
